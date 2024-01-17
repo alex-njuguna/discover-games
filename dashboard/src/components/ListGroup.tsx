d6e5b3a19df398a1e45d10dcc1d1f5f3e2396437
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 01:33:58 +0300

    handling events with type

--- dashboard/src/components/ListGroup.tsx
+++ dashboard/src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let cities = ["Nairobi", "Kampala", "Cape Town", "Marakech", "Dar es Salam"];
-  cities = [];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>Cities</h1>
       {cities.length === 0 && <p>No cities found</p>}
       <ul className="list-group">
         {cities.map((city) => (
-          <li key={city} className="list-group-item">
+          <li key={city} className="list-group-item" onClick={handleClick}>
             {city}
           </li>
         ))}
