3b641c55675471f2663993983786fa4dc7eb44ba
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 00:55:30 +0300

    installing and working with bootstrap

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,11 +1,11 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
-      <Message></Message>
+      <ListGroup></ListGroup>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- dashboard/src/Message.tsx
+++ dashboard/src/Message.tsx
@@ -1,10 +1,7 @@
-
 function Message() {
-
-    const name = 'Alex'
-    if (name)
-        return <h1>Hello, {name}</h1>
-    return <h1>Hello, world</h1>
+  const name = "Alex";
+  if (name) return <h1>Hello, {name}</h1>;
+  return <h1>Hello, world</h1>;
 }
 
-export default Message
+export default Message;
