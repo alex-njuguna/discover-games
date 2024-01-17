1132ca40bc626b8f752eb01063bd2708a81cab02
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 01:10:15 +0300

    rendering javascript list to tsx using map

--- dashboard/src/components/ListGroup.tsx
+++ dashboard/src/components/ListGroup.tsx
@@ -1,13 +1,21 @@
 function ListGroup() {
+  const cities = [
+    "Nairobi",
+    "Kampala",
+    "Cape Town",
+    "Marakech",
+    "Dar es Salam",
+  ];
+
   return (
     <>
-      <h1>List</h1>
+      <h1>Cities</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {cities.map((city) => (
+          <li key={city} className="list-group-item">
+            {city}
+          </li>
+        ))}
       </ul>
     </>
   );
