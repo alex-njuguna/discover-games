c49715820c0825313c542f258c360bd400ff35fc
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 00:32:45 +0300

    basic setup

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <Message></Message>
+    </div>
+  )
+}
+
+export default App
---
+++ dashboard/src/Message.tsx
@@ -0,0 +1,10 @@
+
+function Message() {
+
+    const name = 'Alex'
+    if (name)
+        return <h1>Hello, {name}</h1>
+    return <h1>Hello, world</h1>
+}
+
+export default Message
