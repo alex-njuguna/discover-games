3867f201d2635216ff27eccc59bcf15be356cdf9
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 04:59:07 +0300

    parsing children to as props

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,27 +1,41 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+
+// function App() {
+//   let items = ["Nairobi", "Kampala", "Cape Town", "Marakech", "Dar es Salam"];
+//   let names = ["Mike", "Edith", "Bien", "Bjorn", "Peter"];
+
+//   function handleSelectItem(item: string) {
+//     console.log(item);
+//   }
+
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       ></ListGroup>
+
+//       <ListGroup
+//         items={names}
+//         heading="Names"
+//         onSelectItem={handleSelectItem}
+//       ></ListGroup>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Nairobi", "Kampala", "Cape Town", "Marakech", "Dar es Salam"];
-  let names = ["Mike", "Edith", "Bien", "Bjorn", "Peter"];
-
-  function handleSelectItem(item: string) {
-    console.log(item);
-  }
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
-
-      <ListGroup
-        items={names}
-        heading="Names"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
-    </div>
+    <>
+      <Alert>
+        <h3>The last of the mojicans</h3>
+      </Alert>
+    </>
   );
 }
 
