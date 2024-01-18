1235fea47b109ba5508b55cc4038c5f750fd1c84
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 05:25:28 +0300

    simulation of a button

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,40 +1,15 @@
-// import ListGroup from "./components/ListGroup";
-
-// function App() {
-//   let items = ["Nairobi", "Kampala", "Cape Town", "Marakech", "Dar es Salam"];
-//   let names = ["Mike", "Edith", "Bien", "Bjorn", "Peter"];
-
-//   function handleSelectItem(item: string) {
-//     console.log(item);
-//   }
-
-//   return (
-//     <div>
-//       <ListGroup
-//         items={items}
-//         heading="Cities"
-//         onSelectItem={handleSelectItem}
-//       ></ListGroup>
-
-//       <ListGroup
-//         items={names}
-//         heading="Names"
-//         onSelectItem={handleSelectItem}
-//       ></ListGroup>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+  const handleClick = () => {
+    console.log("hello world");
+  };
+
   return (
     <>
-      <Alert>
-        <h3>The last of the mojicans</h3>
-      </Alert>
+      <Button color="danger" onClick={handleClick}>
+        MyButton
+      </Button>
     </>
   );
 }
