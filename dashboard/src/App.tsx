d711204f88f6df2f0168149172b3bdc9e7dae7c8
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-18 03:07:04 +0300

    passing functions as props

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -4,11 +4,23 @@ function App() {
   let items = ["Nairobi", "Kampala", "Cape Town", "Marakech", "Dar es Salam"];
   let names = ["Mike", "Edith", "Bien", "Bjorn", "Peter"];
 
+  function handleSelectItem(item: string) {
+    console.log(item);
+  }
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities"></ListGroup>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
 
-      <ListGroup items={names} heading="Names"></ListGroup>
+      <ListGroup
+        items={names}
+        heading="Names"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
     </div>
   );
 }
--- dashboard/src/components/ListGroup.tsx
+++ dashboard/src/components/ListGroup.tsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -22,7 +23,10 @@ function ListGroup({ items, heading }: Props) {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
